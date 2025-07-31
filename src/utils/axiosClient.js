5299d81614d869efef06b18029247d171175f59f
Varshith k <kv109215@gmail.com>
2025-07-31 15:30:28 +0530

    Changed axios second time

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const aClient=axios.create({
-    baseURL:"https://finance-backend-txwi-qudr9nffm-varshith-ks-projects-c5fab339.vercel.app/",
+    baseURL:"https://finance-backend-chi.vercel.app/",
     withCredentials:true,
     headers:{
         "Content-Type": "application/json"
